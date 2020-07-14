fb90286cc396e7157808820891210d6121edae0f
Rhodion Souza Araujo <rhodions@gmail.com>
2020-07-14 09:47:23 -0300

    Refatoração

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = (options = 8, personalChars = "") => {
   const numbers = "1234567890";
   const specials = "!@#$%&*()_+-";
 
-  let regex = null;
   let size = 8;
   let getLower = true;
   let getUpper = true;
@@ -14,10 +13,12 @@ module.exports = (options = 8, personalChars = "") => {
   let chars = "";
 
   if (typeof options == "object") {
-    regex =
-      options.regex && options.regex != ""
-        ? new RegExp(options.regex, "g")
-        : regex;
+    if (options.regex && options.regex != "") {
+      const regex = RegExp(options.regex, "g");
+      const randexp = new RandExp(regex);
+      return randexp.gen();
+    }
+
     size = typeof options.size == "number" ? options.size : size;
     getLower =
       options.config && typeof options.config.lower == "boolean"
@@ -41,42 +42,37 @@ module.exports = (options = 8, personalChars = "") => {
     }
   }
 
+  if (getLower) {
+    chars = chars + lower;
+  }
+  if (getUpper) {
+    chars = chars + upper;
+  }
+  if (getNumber) {
+    chars = chars + numbers;
+  }
+  if (getSpecial) {
+    chars = chars + specials;
+  }
+  if (chars.length == 0) {
+    chars = lower + upper + numbers + specials;
+  }
+  if (
+    personalChars &&
+    typeof personalChars == "string" &&
+    personalChars != ""
+  ) {
+    chars = personalChars;
+  }
+
+  chars = chars.split("");
+
   let pass = "";
   let cont = 0;
-  if (regex == null) {
-    if (getLower) {
-      chars = chars + lower;
-    }
-    if (getUpper) {
-      chars = chars + upper;
-    }
-    if (getNumber) {
-      chars = chars + numbers;
-    }
-    if (getSpecial) {
-      chars = chars + specials;
-    }
-    if (chars.length == 0) {
-      chars = lower + upper + numbers + specials;
-    }
-    if (
-      typeof personalChars == "string" &&
-      personalChars &&
-      personalChars != ""
-    ) {
-      chars = personalChars;
-    }
-
-    chars = chars.split("");
-
-    while (cont < size) {
-      var num = parseInt(Math.random() * chars.length);
-      pass = pass + chars[num];
-      cont++;
-    }
-  } else {
-    const randexp = new RandExp(regex);
-    pass = randexp.gen();
+  while (cont < size) {
+    var num = parseInt(Math.random() * chars.length);
+    pass = pass + chars[num];
+    cont++;
   }
 
   return pass;
